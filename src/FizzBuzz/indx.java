9948674e3bbf989ed2b82ef59c8cc08876aa2e97
Keileudo <keilyleudo4@gmail.com>
2024-08-09 18:19:23 -0500

    ejercicio fizz buzz

---
+++ src/FizzBuzz/indx.java
@@ -0,0 +1,26 @@
+package FizzBuzz;
+
+public class indx {
+
+	public static void main(String[] args) {
+		int contador=1;
+		while(contador<=100) {
+			if (contador % 3 == 0 && contador % 5 ==0)
+			{
+				System.out.println("fizzbuzz");
+			}else if(contador % 3 == 0) 
+			{
+				System.out.println("fizz");
+			}
+			else if (contador % 5 == 0) 
+			{
+				System.out.println("buzz");
+			}
+			else {
+				System.out.println(contador);
+			}
+			contador++;
+	}
+
+	}
+}
